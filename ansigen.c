830ccb1437e1d0a27c2f7f0609a5d880835fd100
dcat <dcat@iotek.org>
2020-05-15 15:37:54 +0200

    added arg handling and DBG macro

--- ansigen.c
+++ ansigen.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <X11/Xlib.h>
 #include <Imlib2.h>
+#include <err.h>
+#include "arg.h"
+
+#define DBG(lvl, fmt, ...) if (debug >= lvl) warnx(fmt, __VA_ARGS__)
+static int debug = 0;
 
 int
 main(int argc, char **argv) {
 	Imlib_Image image;
 	int w, h;
+	char *argv0;
 
-	image = imlib_load_image("/tmp/img.png");
-	imlib_context_set_image(image);
-	w = imlib_image_get_width();
-	h = imlib_image_get_height();
-	imlib_context_set_image(image);
-	imlib_free_image();
+	ARGBEGIN {
+	case 'd':
+		debug++;
+		DBG(1, "debugging level %d", debug);
+		break;
+	} ARGEND
 
-	printf("Width: %d Height: %d\n", w, h);
+	while (*argv) {
+		image = imlib_load_image(*argv);
+		imlib_context_set_image(image);
+		w = imlib_image_get_width();
+		h = imlib_image_get_height();
+		DBG(2, "loaded image %s, width: %d, height: %d", *argv, w, h);
+		imlib_context_set_image(image);
+		imlib_free_image();
+
+		(void)*argv++;
+	}
+
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 PREFIX  = /usr
-LDFLAGS = -lX11 -lImlib2
-CFLAGS  = -Wall -pedantic $(LDFLAGS)
+LDFLAGS = -lX11 -lImlib2 -L/opt/X11/lib
+CFLAGS  = -Wall -pedantic -I/opt/X11/include
 
 all: ansigen
